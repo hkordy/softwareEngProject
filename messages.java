8098318ef42bbf41477681ab5379ad0e0377eeec
Hend Kordy <39963473+hkordy@users.noreply.github.com>
2019-12-05 10:47:21 -0800

    Add files via upload

--- messages.java
+++ messages.java
@@ -3,7 +3,7 @@ package BasicClientServer;
 public class messages{
 
 	String message;
-
+	String test;
 	public String getFailedConnection() {
 		return message = "Connection Faild, please try again";
 	}
